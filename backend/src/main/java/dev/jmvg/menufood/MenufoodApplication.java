c255d3c95badfb03084ac4547cd31b0107e84e01
Joanantha Matheus <jmvg.comp@gmail.com>
2021-09-30 14:27:17 -0300

    backend commit initial

---
+++ backend/src/main/java/dev/jmvg/menufood/MenufoodApplication.java
@@ -0,0 +1,13 @@
+package dev.jmvg.menufood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenufoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenufoodApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/dev/jmvg/menufood/MenufoodApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jmvg.menufood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenufoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
